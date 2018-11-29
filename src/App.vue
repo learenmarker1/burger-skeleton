9cc73f07aa97ff38b5aefeab8fd847b0953452a9
Elsa Jerhamre <elsajerhamre@Elsas-MacBook-Pro.local>
2018-11-29 11:13:10 +0100

    ändrat bakgrund /Elsa

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,9 @@ export default {
 <style>
 body {
   height:100vh;
-  background: radial-gradient(white, lightblue);
+  background: radial-gradient(lightgray, darkgray);
   background-size: 200% 200%;
-  animation: Gradient 20s linear infinite;
+  /*animation: Gradient 20s linear infinite;*/
 }
 
 @keyframes Gradient {
