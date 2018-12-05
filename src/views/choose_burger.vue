1b2418ae523e45d96757e388f61d6102302ae2b5
HannaLycken <Hanna.Lycken.4861@student.uu.se>
2018-12-05 09:50:40 +0100

    flyttat ordering till choose_burger

--- src/views/choose_burger.vue
+++ src/views/choose_burger.vue
@@ -14,10 +14,16 @@
 
     <div class="panel">
       <div class="panel-title" v-on:click="sel == 1 ? sel = 0 : sel = 1">
-        BUN
+        <img src='http://al-taiclub.com/images/icons-burger-clipart-2.png'  width=30>
+        {{ uiLabels.bun }}
+
+        <div id="yourOrder">
+          {{ uiLabels.bunChoice }}
+          {{chosenIngredients.map(item => item["ingredient_"+lang]).join(', ') }}
+        </div>
+
       </div>
       <div class="panel-body" v-show="sel == 1">
-        <button v-on:click="deleteFromOrder(item)"> delete</button>
         <Ingredient
         ref="ingredient"
         v-for="item in ingredients"
@@ -27,14 +33,20 @@
         :item="item"
         :lang="lang"
         :key="item.ingredient_id">
-
       </Ingredient>
-
     </div>
   </div>
+
+
   <div class="panel">
     <div class="panel-title" v-on:click="sel == 2 ? sel = 0 : sel = 2">
-      PATTY
+      <img src='http://al-taiclub.com/images/icons-burger-clipart-2.png'  width=30>
+      {{ uiLabels.patty }}
+      <div id="yourOrder">
+        {{ uiLabels.pattyChoice }}
+        {{chosenIngredients.map(item => item["ingredient_"+lang]).join(', ') }}
+      </div>
+
     </div>
     <div class="panel-body" v-show="sel == 2">
       <Ingredient
@@ -49,9 +61,15 @@
     </Ingredient>
   </div>
 </div>
+
 <div class="panel">
   <div class="panel-title" v-on:click="sel == 3 ? sel = 0 : sel = 3">
-    TOPPINGS
+    <img src='http://al-taiclub.com/images/icons-burger-clipart-2.png'  width=30>
+    {{ uiLabels.topping }}
+    <div id="yourOrder">
+      {{ uiLabels.toppingChoice }}
+      {{chosenIngredients.map(item => item["ingredient_"+lang]).join(', ') }}
+    </div>
   </div>
   <div class="panel-body" v-show="sel == 3">
     <Ingredient
@@ -66,9 +84,15 @@
   </Ingredient>
 </div>
 </div>
+
 <div class="panel">
   <div class="panel-title" v-on:click="sel == 4 ? sel = 0 : sel = 4">
-    SAUCE
+    <img src='http://al-taiclub.com/images/icons-burger-clipart-2.png'  width=30>
+    {{ uiLabels.sauce }}
+    <div id="yourOrder">
+      {{ uiLabels.sauceChoice }}
+      {{chosenIngredients.map(item => item["ingredient_"+lang]).join(', ') }}
+    </div>
   </div>
   <div class="panel-body" v-show="sel == 4">
     <Ingredient
@@ -84,11 +108,18 @@
 </div>
 </div>
 
+
+
 <h1 align="center">{{ uiLabels.sideOrder }}</h1>
 
 <div class="panel">
   <div class="panel-title" v-on:click="sel == 6 ? sel = 0 : sel = 6">
-    TILLBEHÖR
+    <img src='http://al-taiclub.com/images/icons-burger-clipart-2.png'  width=30>
+    {{ uiLabels.sideorders }}
+    <div id="yourOrder">
+      {{ uiLabels.addingsChoice }}
+      {{chosenIngredients.map(item => item["ingredient_"+lang]).join(', ') }}
+    </div>
   </div>
   <div class="panel-body" v-show="sel == 6">
     <Ingredient
@@ -105,7 +136,12 @@
 </div>
 <div class="panel">
   <div class="panel-title" v-on:click="sel == 7 ? sel = 0 : sel = 7">
-    DRINKS
+    <img src='https://techflourish.com/images/hot-dog-chips-and-drink-clipart-18.png'  width=30>
+    {{ uiLabels.drinks }}
+    <div id="yourOrder">
+      {{ uiLabels.drinksChoice }}
+      {{chosenIngredients.map(item => item["ingredient_"+lang]).join(', ') }}
+    </div>
   </div>
   <div class="panel-body" v-show="sel == 7">
     <Ingredient
@@ -139,15 +175,6 @@
   :key="key">
 </OrderItem>
 </div>
-
-<div>
-  <button id = "backButton" onclick="window.location = '/#/start';"> {{ uiLabels.backButton }} </button>
-</div>
-
-<div>
-  <button align="right" id = "checkoutButton" onclick="window.location = '/#/checkout';"> {{ uiLabels.checkoutButton }} </button>
-</div>
-
 <!-- <p> Estimated time: {{this.orderNumber}} </p> -->
 </div>
 </body>
@@ -198,6 +225,7 @@ export default {
       this.chosenIngredients.splice(this.chosenIngredients.indexOf(item),1);
       this.price += -item.selling_price;
     },
+
     placeOrder: function () {
       var i,
       //Wrap the order in an object
@@ -214,7 +242,6 @@ export default {
       this.price = 0;
       this.chosenIngredients = [];
     },
-
     next: function () {
       this.showCategory += 1;
     },
@@ -226,6 +253,7 @@ export default {
         document.getElementById("langPic").src = "https://cdn.pixabay.com/photo/2017/01/31/16/46/banner-2025451__340.png";
       }
     }
+
   }
 }
 window.onscroll = function() {scrollFunction()};
@@ -239,9 +267,6 @@ function scrollFunction() {
 }
 
 </script>
-
-
-
 <style scoped>
 /* scoped in the style tag means that these rules will only apply to elements, classes and ids in this template and no other templates. */
 
@@ -265,7 +290,7 @@ function scrollFunction() {
 #ordering {
   margin: auto;
   padding-top: 150px;
-  width: 47em;
+  max-width: 47em;
   background: radial-gradient(lightgray, darkgray);
   color: white;
 }
@@ -275,6 +300,11 @@ function scrollFunction() {
   top: 8px;
   right: 16px;
 }
+
+#yourOrder{
+  text-align: right;
+}
+
 .ingredient {
   border: 1px solid #ccd;
   padding: 1em;
