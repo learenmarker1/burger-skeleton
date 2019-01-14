f1f9b1705d282492bca9016a2427f1ee0690dc1f
HannaLycken <Hanna.Lycken.4861@student.uu.se>
2019-01-14 11:32:23 +0100

    fixat bugg

--- src/views/checkout.vue
+++ src/views/checkout.vue
@@ -50,7 +50,6 @@
 import checkoutComponent from '@/components/checkoutComponent.vue'
 //import methods and data that are shared between ordering and kitchen views
 import sharedVueStuff from '@/components/sharedVueStuff.js'
-import choose_burger from '@/views/choose_burger.vue'
 
 
 export default {
@@ -70,9 +69,7 @@ export default {
     },
 
     placeOrder: function () {
-      var i,
-      //Wrap the order in an object
-      order = {
+      var order = {
         burgers: this.burgers
       };
       // make use of socket.io's magic to send the stuff to the kitchen via the server (app.js)
